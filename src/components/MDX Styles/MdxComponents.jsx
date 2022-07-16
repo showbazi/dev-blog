90ef9a82886078b5ea7aa7d452568ac1c22a1724
showbazi <mail.bsagar@gmail.com>
2022-07-16 20:56:19 +0530

    some bug fixing

--- src/components/MDX Styles/MdxComponents.jsx
+++ src/components/MDX Styles/MdxComponents.jsx
@@ -1,9 +1,8 @@
 import codes from './Codes';
-import embeddedComponents from './Embeds';
 import Images from './Images';
 import lists from './Lists';
 import Texts from './Texts';
 
-const MdxComponents = {...Texts, ...embeddedComponents, ...codes, ...lists, ...Images};
+const MdxComponents = {...Texts, ...codes, ...lists, ...Images};
 
 export default MdxComponents;
